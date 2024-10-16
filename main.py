0fe36750d0526661e3b669c31342723a8f93f5cf
IonesioJunior <ionesiojr@gmail.com>
2024-10-16 12:32:37 -0300

    Update main.py to follow the new folder structures

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import os
 import json
 from datetime import datetime
-from syftbox.lib import ClientConfig
+from syftbox.lib import Client, SyftPermission
+
 
 def main():
     # Load the client configuration
-    config_path = os.environ.get("SYFTBOX_CLIENT_CONFIG_PATH", None)
-    client_config = ClientConfig.load(config_path)
+    # config_path = os.environ.get("SYFTBOX_CLIENT_CONFIG_PATH", None)
+    client_config = ClientConfig.load()
 
     # Get the current timestamp
     current_timestamp = datetime.now().isoformat()
 
     # Prepare the data to be written
-    timestamp_data = {
-        "last_check_in": current_timestamp
-    }
+    timestamp_data = {"last_check_in": current_timestamp}
 
     # Prepare output folders
     output_folder = f"{client_config.sync_folder}/{client_config.email}/app_pipelines/timestamp_recorder/"
@@ -31,7 +30,7 @@ def main():
         "read": ["GLOBAL"],
         "write": [client_config.email],
         "filepath": f"{output_folder}_.syftperm",
-        "terminal": False
+        "terminal": False,
     }
     syftperm_path = f"{output_folder}_.syftperm"
     with open(syftperm_path, "w") as f:
@@ -39,6 +38,7 @@ def main():
 
     print(f"Timestamp has been written to {output_file_path}")
     print(f"_.syftperm file has been written to {syftperm_path}")
+
 
 if __name__ == "__main__":
     main()
