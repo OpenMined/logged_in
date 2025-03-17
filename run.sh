539232d945135440b6796950b3faab9fa5e8fa6d
Yash Gorana <yash.gorana@hotmail.com>
2025-03-17 14:00:23 +0530

    run.sh - setup once

--- run.sh
+++ run.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 set -e
 
+uv venv -p 3.12 .venv
+uv pip install -U syftbox
+. .venv/bin/activate
+
 while true; do
-    if [ ! -d ".venv" ]; then
-        echo "Virtual environment not found. Creating one..."
-        uv venv -p 3.12 .venv
-        echo "Virtual environment created successfully."
-    else
-        echo "Virtual environment already exists."
-    fi
-
-    uv pip install -U syftbox
-
-    . .venv/bin/activate
-
     echo "Running 'logged_in' with $(python3 --version) at '$(which python3)'"
     uv run python3 main.py
-
-    deactivate
 
     echo "Sleeping for 10 seconds..."
     sleep 10
 done
 
+deactivate
